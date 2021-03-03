e85eb191c77533ba7e59a5453966fa9d5aa0c12a
abzdev <abzdvlpmnt@gmail.com>
2021-03-02 18:49:18 -0800

    add database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
-var ball;
+var hypnoticBall, database, position;
 
-function setup(){
+function setup() {
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+
+    database = firebase.database();
+    console.log(database);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+    var hypnoticBallPosition = database.ref('ball/position');
+    hypnoticBallPosition.on('value',readPosition,showError);
 }
 
-function draw(){
+function draw() {
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+    if(position !== 'undefined') {
+        if(keyDown(LEFT_ARROW)) {
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)) {
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)) {
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)) {
+            writePosition(0,1);
+        }
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y) {
+    database.ref('ball/position').set({'x':position.x + x,'y':position.y + y});
+}
+function readPosition(data) {
+    position = data.val();
+    console.log(position);
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
+function showError() {
+    console.log('ERROR');
 }
